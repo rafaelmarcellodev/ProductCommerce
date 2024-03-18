8bed253eb04f9c1627fd3b86d9ad0e4f4ebe70ea
Rafael Marcello <rafael.marcello.dev@gmail.com>
2024-03-18 13:48:33 -0300

    Fix: service exception

--- src/ProductAPI/Services/ExceptionHandlingService.cs
+++ src/ProductAPI/Services/ExceptionHandlingService.cs
@@ -2,7 +2,7 @@
 {
     public class ExceptionHandlingService
     {
-        public string HandleException(Exception ex)
+        public virtual string HandleException(Exception ex)
         {
             //TODO Add ILogger
             return "Ocorreu um erro durante o processamento da solicitação. Por favor, tente novamente mais tarde.";
