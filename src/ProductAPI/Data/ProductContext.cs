91ed9202cefa232a04c30d4e0f2a9dc6800fb3aa
Rafael Marcello <rafael.marcello.dev@gmail.com>
2024-03-18 08:23:11 -0300

    Add context product and connection string

---
+++ src/ProductAPI/Data/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Data.Models;
+
+namespace ProductAPI.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
--- src/ProductAPI/Program.cs
+++ src/ProductAPI/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ProductContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("ProductConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
