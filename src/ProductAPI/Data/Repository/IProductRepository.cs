629ef62ee75075926bbb4d6f2b686ee4ebea30c1
Rafael Marcello <rafael.marcello.dev@gmail.com>
2024-03-18 08:32:42 -0300

    Add Repository

---
+++ src/ProductAPI/Data/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProductAPI.Data.Models;
+
+namespace ProductAPI.Data.Repository
+{
+    public interface IProductRepository
+    {
+        Task<Product> AddProduct(Product product);
+        Task<List<Product>> GetProducts();
+        Task<Product?> GetProductById(int id);
+        Task<List<Product>> GetProductByName(string name);
+        Task<Product> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(int id);
+    }
+}
---
+++ src/ProductAPI/Data/Repository/ProductRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Data.Models;
+
+namespace ProductAPI.Data.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _context;
+
+        public ProductRepository(ProductContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product?> GetProductById(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<List<Product>> GetProductByName(string name)
+        {
+            var products = _context.Products.Where(p => p.Name.ToLower().Contains(name.ToLower()));
+
+            return await products.ToListAsync();
+        }
+
+        public async Task<List<Product>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> UpdateProduct(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<bool> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return false;
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
