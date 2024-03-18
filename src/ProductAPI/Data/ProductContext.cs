ca8550782f14c3c98128d468b9d7ab80f1d7ec73
Rafael Marcello <rafael.marcello.dev@gmail.com>
2024-03-18 09:40:41 -0300

    Add seed product

--- src/ProductAPI/Data/ProductContext.cs
+++ src/ProductAPI/Data/ProductContext.cs
@@ -10,5 +10,46 @@ namespace ProductAPI.Data
         }
 
         public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Product 1",
+                    Stock = 10,
+                    Price = 12.50m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Product 2",
+                    Stock = 20,
+                    Price = 15.75m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Product 3",
+                    Stock = 15,
+                    Price = 10.00m
+                },
+                new Product
+                {
+                    Id = 4,
+                    Name = "Product 4",
+                    Stock = 8,
+                    Price = 18.25m
+                },
+                new Product
+                {
+                    Id = 5,
+                    Name = "Product 5",
+                    Stock = 25,
+                    Price = 9.99m
+                }
+                );
+        }
     }
 }
