bd7d01b26c2be5834610d56ee25a05741f9673c2
Rafael Marcello <rafael.marcello.dev@gmail.com>
2024-03-18 08:07:36 -0300

    Add model product

---
+++ src/ProductAPI/Data/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductAPI.Data.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+    }
+}
